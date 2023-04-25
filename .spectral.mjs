4e2ea17944a1b0d3ff7c203f0a243ce4c274ada4
Simon <admin@wolkeneis.dev>
2023-04-25 21:45:04 +0000

    Added 3 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjE5NzQ1NA/spectral.js?branch_slug=main&token=952676e1-f9f8-46fd-8d5d-6d42b3a08f2b";
+export default { extends: ruleset };

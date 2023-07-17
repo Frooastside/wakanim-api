aa4f1a14d582b3b900ccec8691dd95b3633a07c1
Simon <47527944+Frooastside@users.noreply.github.com>
2023-07-17 12:10:07 +0200

    add basic type definition setup

---
+++ types/index.ts
@@ -0,0 +1,4 @@
+import type { wakanim_api } from "./wakanim_api";
+import type { wakanim_core } from "./wakanim_core";
+
+export type { wakanim_api, wakanim_core };
---
+++ types/wakanim_core.ts
@@ -0,0 +1,9 @@
+import type { components as _components, external as _external, operations as _operations, paths as _paths } from "../reference/wakanim_core";
+
+declare namespace wakanim_core {
+  export type external = _external;
+  export type operations = _operations;
+  export type paths = _paths;
+}
+
+export type { wakanim_core };
